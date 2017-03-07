524213cd6b790378ef244435de2f54fe94652399
jmkluthe <kluthej@oregonstate.edu>
2017-03-06 22:56:50 -0800

    add docstrings and refactoring

--- traveling_salesman_problem/__main__.py
+++ traveling_salesman_problem/__main__.py
@@ -18,18 +18,35 @@ def write_output_file(filename, out_data):
 
 
 def compute_edge_lists(in_data):
-    edges = {}
+    """
+    Computes the edge lists for a complete graph where the nodes are represented by (x, y) point.
+    The edge list is for each node consists of dictionary entries with the key being the node number of every other
+    point in the graph, and the value being the Euclidean distance between the two nodes.
+
+    :param in_data: dictionary(key: tuple) - key is the node number, tuple is (x, y) position.
+    :return edge_lists: dictionary(key: dictionary) - key is the node number, dictionary is the set of other nodes and
+        their distances from the first node.
+    """
+    edge_lists = {}
     for u in in_data.keys():
-        edges[u] = {}
+        edge_lists[u] = {}
         for v in in_data.keys():
             if u != v:
-                edges[u][v] = compute_edge_weight(in_data(u), in_data(v))
+                edge_lists[u][v] = compute_edge_weight(in_data[u], in_data[v])
+    return edge_lists
 
 
 def compute_edge_weight(u, v):
-    delta_x_squared = float((u[0] - v[0])**2)
-    delta_y_squared = float((u[1] - v[1])**2)
-    return round((delta_x_squared + delta_y_squared)**(0.5))
+    """
+    Calculates and returns Euclidean distance between two points in 2 dimensions.
+
+    :param u: tuple of ints representing (x,y) position of a node
+    :param v: tuple of ints representing (x,y) position of a node
+    :return: int distance between points u and v
+    """
+    delta_x_squared = (u[0] - v[0]) ** 2
+    delta_y_squared = (u[1] - v[1]) ** 2
+    return round((delta_x_squared + delta_y_squared) ** 0.5)
 
 
 if len(argv) == 2:
@@ -38,5 +55,8 @@ else:
     filename = "tsp_example_1.txt"
 in_data = read_input_file(filename)
 print(in_data)
+edge_lists = compute_edge_lists(in_data)
+for node,edge_list in edge_lists.items():
+    print("{}: {}".format(node, edge_list))
 
 
